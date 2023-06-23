6842dee7ad135dd726d85cce7e305f9de1b3c76f
Petko <petko.chepishev+corespaces@nagarro.com>
2023-06-23 19:51:15 +0300

    minor changes

--- doc_to_llm.py
+++ doc_to_llm.py
@@ -1,13 +1,12 @@
 from qa_chain import create_qa_chain
 from file_downloader import text_splitter
 from embeddings import create_embeddings
+from dotenv import load_dotenv
 import os
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "HUGGINGFACEHUB_API_TOKEN"
+load_dotenv()
 
-url = "https://raw.githubusercontent.com/hwchase17/chat-your-data/master/state_of_the_union.txt"
-doc_name = "state_of_the_union.txt"
 query = "What did the president say about the Supreme Court"
 
-db = create_embeddings
-docs = text_splitter(url, doc_name)
+docs = text_splitter(os.getenv("URL"), os.getenv("DOC_NAME"))
+db = create_embeddings(docs)
 create_qa_chain(db, query)
--- qa_chain.py
+++ qa_chain.py
@@ -1,10 +1,12 @@
 from langchain.chains.question_answering import load_qa_chain
 from langchain import HuggingFaceHub
-
+import os
 
 def create_qa_chain(db, query):
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl",
-                         model_kwargs={"temperature": 0.15, "max_length": 512})
+                         model_kwargs={
+                             "temperature": os.getenv("MODEL_TEMPERATURE"),
+                             "max_length": os.getenv("MODEL_MAX_LENGTH")})
 
     chain = load_qa_chain(llm, chain_type="mydoc")
     docs = db.similarity_search(query)
